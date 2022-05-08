ef6bd09c9172f2975cfe88c8ad621cd0ad86c538
Naysay3r <ilia.mahnach@gmail.com>
2022-05-08 14:46:08 +0300

    add: add keys.js

---
+++ src/keyboard/keys.js
@@ -0,0 +1,352 @@
+export const keyboard_tpl = [
+    ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'NumpadSubstract', 'Equal', 'Backspace'],
+    ['Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash'],
+    ['CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter'],
+    ['ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Backquote', 'Period', 'Slash', 'ShiftRight'],
+    ['Control', 'OSLeft', 'AltLeft', 'Space', 'AltRight', 'OSRight', 'ControlRight']
+];
+
+export const keys = {
+    'Backquote' : {
+        'key': '`',
+        'shift_key': '~',
+        'rukey': 'ё',
+        'shift_rukey': 'Ё',
+    },
+    'Digit1' : {
+        'key': '1',
+        'shift_key': '!',
+        'rukey': '1',
+        'shift_rukey': '!',
+    },
+    'Digit2' : {
+        'key': '2',
+        'shift_key': '@',
+        'rukey': '2',
+        'shift_rukey': '"',
+    },
+    'Digit3' : {
+        'key': '3',
+        'shift_key': '#',
+        'rukey': '3',
+        'shift_rukey': '№',
+    },
+    'Digit4' : {
+        'key': '4',
+        'shift_key': '$',
+        'rukey': '4',
+        'shift_rukey': '$',
+    },
+    'Digit5' : {
+        'key': '5',
+        'shift_key': '%',
+        'rukey': '5',
+        'shift_rukey': '%',
+    },
+    'Digit6' : {
+        'key': '6',
+        'shift_key': '^',
+        'rukey': '6',
+        'shift_rukey': ':',
+    },
+    'Digit7' : {
+        'key': '7',
+        'shift_key': '&',
+        'rukey': '7',
+        'shift_rukey': '?',
+    },
+    'Digit8' : {
+        'key': '8',
+        'shift_key': '*',
+        'rukey': '8',
+        'shift_rukey': '*',
+    },
+    'Digit9' : {
+        'key': '9',
+        'shift_key': '(',
+        'rukey': '9',
+        'shift_rukey': '(',
+    },
+    'Digit0' : {
+        'key': '0',
+        'shift_key': ')',
+        'rukey': '0',
+        'shift_rukey': ')',
+    },
+    'NumpadSubstract' : {
+        'key': '-',
+        'shift_key': '_',
+        'rukey': '-',
+        'shift_rukey': '_',
+    },
+    'Equal' : {
+        'key': '=',
+        'shift_key': '+',
+        'rukey': '=',
+        'shift_rukey': '+',
+    },
+    'Backspace' : {
+        'key': 'Backspace',
+        'dark': true
+    },
+    //     ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'NumpadSubstract', 'Equal', 'Backspace'],
+
+    'Tab' : {
+        'key': 'Tab',
+        'dark': true
+    },
+    'KeyQ' : {
+        'key': 'q',
+        'shift_key': 'Q',
+        'rukey': 'й',
+        'shift_rukey': 'Й',
+    },
+    'KeyW' : {
+        'key': 'w',
+        'shift_key': 'W',
+        'rukey': 'ц',
+        'shift_rukey': 'Ц',
+    },
+    'KeyE' : {
+        'key': 'e',
+        'shift_key': 'E',
+        'rukey': 'у',
+        'shift_rukey': 'У',
+    },
+    'KeyR' : {
+        'key': 'r',
+        'shift_key': 'R',
+        'rukey': 'к',
+        'shift_rukey': 'К',
+    },
+    'KeyT' : {
+        'key': 't',
+        'shift_key': 'T',
+        'rukey': 'е',
+        'shift_rukey': 'Е',
+    },
+    'KeyY' : {
+        'key': 'y',
+        'shift_key': 'Y',
+        'rukey': 'н',
+        'shift_rukey': 'Н',
+    },
+    'KeyU' : {
+        'key': 'u',
+        'shift_key': 'U',
+        'rukey': 'г',
+        'shift_rukey': 'Г',
+    },
+    'KeyI' : {
+        'key': 'i',
+        'shift_key': 'I',
+        'rukey': 'ш',
+        'shift_rukey': 'Ш',
+    },
+    'KeyO' : {
+        'key': 'o',
+        'shift_key': 'O',
+        'rukey': 'щ',
+        'shift_rukey': 'Щ',
+    },
+    'KeyP' : {
+        'key': 'p',
+        'shift_key': 'P',
+        'rukey': 'з',
+        'shift_rukey': 'З',
+    },
+    'BracketLeft' : {
+        'key': '[',
+        'shift_key': '{',
+        'rukey': 'х',
+        'shift_rukey': 'Х',
+    },
+    'BracketRight' : {
+        'key': ']',
+        'shift_key': '}',
+        'rukey': 'ъ',
+        'shift_rukey': 'Ъ',
+    },
+    'Backslash' : {
+        'key': '\\',
+        'shift_key': '|',
+        'rukey': '\\',
+        'shift_rukey': '/',
+    },
+    //['Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash'],
+    'CapsLock' : {
+        'key': 'CapsLock',
+        'dark': true
+    },
+    'KeyA' : {
+        'key': 'a',
+        'shift_key': 'A',
+        'rukey': 'ф',
+        'shift_rukey': 'Ф',
+    },
+    'KeyS' : {
+        'key': 's',
+        'shift_key': 'S',
+        'rukey': 'ы',
+        'shift_rukey': 'Ы',
+    },
+    'KeyD' : {
+        'key': 'd',
+        'shift_key': 'D',
+        'rukey': 'в',
+        'shift_rukey': 'В',
+    },
+    'KeyF' : {
+        'key': 'f',
+        'shift_key': 'F',
+        'rukey': 'а',
+        'shift_rukey': 'А',
+    },
+    'KeyG' : {
+        'key': 'g',
+        'shift_key': 'G',
+        'rukey': 'п',
+        'shift_rukey': 'П',
+    },
+    'KeyH' : {
+        'key': 'h',
+        'shift_key': 'H',
+        'rukey': 'р',
+        'shift_rukey': 'Р',
+    },
+    'KeyK' : {
+        'key': 'k',
+        'shift_key': 'K',
+        'rukey': 'л',
+        'shift_rukey': 'Л',
+    },
+    'KeyL' : {
+        'key': 'l',
+        'shift_key': 'L',
+        'rukey': 'д',
+        'shift_rukey': 'Д',
+    },
+    'Semicolon' : {
+        'key': ';',
+        'shift_key': ':',
+        'rukey': 'ж',
+        'shift_rukey': 'Ж',
+    },
+    'Quote' : {
+        'key': "\'",
+        'shift_key': '\"',
+        'rukey': 'э',
+        'shift_rukey': 'Э',
+    },
+    'Enter' : {
+        'key': 'Enter',
+        'dark': true
+    },
+    // ['CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter'],
+
+    'ShiftLeft' : {
+        'key': 'Shift',
+        'dark': true
+    },
+    'KeyZ' : {
+        'key': 'z',
+        'shift_key': 'Z',
+        'rukey': 'я',
+        'shift_rukey': 'Я',
+    },
+    'KeyX' : {
+        'key': 'x',
+        'shift_key': 'X',
+        'rukey': 'ч',
+        'shift_rukey': 'Ч',
+    },
+    'KeyC' : {
+        'key': 'c',
+        'shift_key': 'C',
+        'rukey': 'с',
+        'shift_rukey': 'С',
+    },
+    'KeyV' : {
+        'key': 'v',
+        'shift_key': 'V',
+        'rukey': 'м',
+        'shift_rukey': 'М',
+    },
+    'KeyB' : {
+        'key': 'b',
+        'shift_key': 'B',
+        'rukey': 'и',
+        'shift_rukey': 'И',
+    },
+    'KeyN' : {
+        'key': 'n',
+        'shift_key': 'N',
+        'rukey': 'т',
+        'shift_rukey': 'Т',
+    },
+    'KeyM' : {
+        'key': 'm',
+        'shift_key': 'M',
+        'rukey': 'ь',
+        'shift_rukey': 'Ь',
+    },
+    'Backquote' : {
+        'key': ',',
+        'shift_key': '<',
+        'rukey': 'б',
+        'shift_rukey': 'Б',
+    },
+    'Period' : {
+        'key': '.',
+        'shift_key': '>',
+        'rukey': 'ю',
+        'shift_rukey': 'Ю',
+    },
+    'Slash' : {
+        'key': '/',
+        'shift_key': '?',
+        'rukey': '.',
+        'shift_rukey': ',',
+    },
+    'ShiftLeft' : {
+        'key': '/',
+        'shift_key': '?',
+        'rukey': '.',
+        'shift_rukey': ',',
+    },
+    'ShiftRight' : {
+        'key': 'Shift',
+        'dark': true
+    },
+    // ['ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Backquote', 'Period', 'Slash', 'ShiftRight'],
+
+    'Control' : {
+        'key': 'Ctrl',
+        'dark': true
+    },
+    'OSLeft' : {
+        'key': 'Win',
+        'dark': true
+    },
+    'AltLeft' : {
+        'key': 'Alt',
+        'dark': true
+    },
+    'Space' : {
+        'key': ' ',
+        'dark': true
+    },
+    'AltRight' : {
+        'key': 'Alt',
+        'dark': true
+    },
+    'OSRight' : {
+        'key': 'Win',
+        'dark': true
+    },
+    'ControlRight' : {
+        'key': 'Ctrl',
+        'dark': true
+    }
+    // ['Control', 'OSLeft', 'AltLeft', 'Space', 'AltRight', 'OSRight', 'ControlRight']
+}
